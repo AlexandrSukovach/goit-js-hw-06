74feef7aedcb1d8b8c37135393a667ccff70355c
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-11-25 15:03:38 +0200

    task -02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const navEl = document.querySelector('#ingredients');
+
+const elementEl = ingredients.map(elemet => {
+  const nemElementEl = document.createElement('li');
+  nemElementEl.classList.add('item');
+  nemElementEl.textContent = elemet;
+  return nemElementEl;
+});
+
+navEl.append(...elementEl);
