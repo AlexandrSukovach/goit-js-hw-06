750ee2636cdf7de818222d473224f4be950807d5
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-11-26 22:07:48 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
+
+
+
+// const nameBgColor = document.querySelector('.color');
+// const changeBgColor = document.querySelector('.change-color');
+
+// changeBgColor.addEventListener('click', clickButtonBgColor);
+// changeBgColor.addEventListener('click', getRandomHexColor);
+
+
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btn.onclick = function () {
+  const rndCol = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = rndCol;
+  spanColor.textContent = rndCol;
+};
