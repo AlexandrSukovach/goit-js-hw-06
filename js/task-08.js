b91dbc76bc70757689e54a3fe5ec136e18233bd1
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-11-29 15:38:24 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector('.login-form');
+
+form.setAttribute("id", "reset");
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+   event.preventDefault();
+
+   const formElements = event.currentTarget.elements;
+   const Email = formElements.email.value;
+   const Password = formElements.password.value;
+
+   const formValue = {
+      Email,
+      Password,
+   };
+   if (Email === '' || Password === '') {
+      alert('Please fill in the field');
+   } else {
+      console.log(formValue);
+      document.getElementById("reset").reset();
+   };
+};
