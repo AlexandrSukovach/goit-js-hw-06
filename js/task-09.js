eba332de1967a0ed4e8c57a3f3df8f379d80128d
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-11-30 12:28:32 +0200

    task-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,13 @@
 
-
-
-
-// const nameBgColor = document.querySelector('.color');
-// const changeBgColor = document.querySelector('.change-color');
-
-// changeBgColor.addEventListener('click', clickButtonBgColor);
-// changeBgColor.addEventListener('click', getRandomHexColor);
-
-
 const btn = document.querySelector('.change-color');
 const spanColor = document.querySelector('.color');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
 btn.onclick = function () {
-  const rndCol = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const rndCol = getRandomHexColor();
   document.body.style.backgroundColor = rndCol;
   spanColor.textContent = rndCol;
 };
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonCreate = document.querySelector('[data-create]');
+const buttonDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+buttonCreate.addEventListener('click', renderAmout);
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+function renderAmout() {
+  const amount = + document.querySelector('#controls>input').value;
+  console.log(amount);
+  createBoxes(amount);
+};
+
+function createBoxes(amount) {
+  const sizeBasic = 30;
+  let divBoxes = document.createDocumentFragment();
+  for (let i = 0; i < amount; i += 1) {
+    let sizeBoxes = sizeBasic + i * 10;
+    let color = getRandomHexColor();
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${sizeBoxes}px; height: ${sizeBoxes}px; background-color: ${color};`;
+    console.log(sizeBoxes);
+    console.log(color);
+    divBoxes.appendChild(div);
+  };
+  boxes.appendChild(divBoxes);
+};
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+};
