52b13568105bf71b3c0fec317b257ce64759b0ed
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-02 16:22:05 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -5,38 +5,35 @@ function getRandomHexColor() {
 const buttonCreate = document.querySelector('[data-create]');
 const buttonDestroy = document.querySelector('[data-destroy]');
 const boxes = document.querySelector('#boxes');
-const input = document.querySelector('input');
+// const input = document.querySelector('input');
 
 let sizeBasic = 30;
+
 buttonCreate.addEventListener('click', renderAmout);
 buttonDestroy.addEventListener('click', destroyBoxes);
 
 function renderAmout() {
   const amount = + document.querySelector('#controls>input').value;
-  console.log(amount);
   createBoxes(amount);
 };
 
 function createBoxes(amount) {
   let divBoxes = document.createDocumentFragment();
   for (let i = 0; i < amount; i += 1) {
-    // let sizeBoxes = sizeBasic + i * 10;
     let sizeBoxes = sizeBasic;
-    sizeBasic += 10
+    sizeBasic += 10;
     let color = getRandomHexColor();
     let div = document.createElement("div");
     div.style.cssText = `width: ${sizeBoxes}px; height: ${sizeBoxes}px; background-color: ${color};`;
-    // console.log(sizeBoxes);
-    // console.log(color);
-    // console.log(sizeBasic);
     divBoxes.appendChild(div);
   };
   boxes.appendChild(divBoxes);
-
 };
 
 function destroyBoxes() {
   boxes.innerHTML = '';
-  input.value = '';
+  // input.value = '';
+  // =======заменил строки 35 и 8========
+  document.querySelector('#controls>input').value = '';
   sizeBasic = 30;
 };
